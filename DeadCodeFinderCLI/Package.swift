6f093e782a472c09e405b98f5743db63cdda078e
Alois Camenzind <aloisc@gmail.com>
2025-09-08 10:12:30 -0500

    a version that ocmpiles but crashes

--- DeadCodeFinderCLI/Package.swift
+++ DeadCodeFinderCLI/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,11 +9,8 @@ let package = Package(
         .macOS(.v12)
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"), // Loosened version for better compatibility
-        
-        // --- THIS IS THE FIX ---
-        // We are changing `from: "1.0.0"` to `branch: "main"` because the repository does not use version tags.
-        .package(url: "https://github.com/swiftlang/indexstore-db.git", branch: "main")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+        .package(url: "https://github.com/swiftlang/indexstore-db.git", branch: "release/6.1.1")
     ],
     targets: [
         .executableTarget(
